910c8cfe40613c57c218c48bde1b65e2c01308bc
Arush Sharma <archer@Arushs-MacBook-Air.local>
2025-04-09 11:14:12 +0530

    fix project card

--- components/Project.tsx
+++ components/Project.tsx
@@ -19,8 +19,8 @@ export default function Project({
   techStack: ReactElement[];
 }) {
   return (
-    <div className="rounded-md overflow-hidden hover:scale-105 duration-300 justify-between cursor-pointer w-[400px] h-[400px] mt-12 bg-slate-100 shadow-[0px_3px_30px_0px_#44337a] flex flex-col items-center gap-8">
-      <div className="relative group overflow-hidden w-full h-[600px]">
+    <div className="rounded-md overflow-hidden hover:scale-105 duration-300 justify-between cursor-pointer w-[400px] h-[500px] mt-12 bg-slate-100 shadow-[0px_3px_30px_0px_#44337a] flex flex-col items-center gap-8">
+      <div className="flex-1 group">
         <Image
           src={mainImage}
           width={400}
@@ -30,34 +30,38 @@ export default function Project({
         />
 
         {/* Black gradient overlay from top (transparent) to bottom (black) */}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-80 transition-opacity duration-300" />
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-0 group-hover:opacity-90 transition-opacity duration-300 flex flex-col gap-8 items-center justify-end pb-8">
+          {/* GitHub Icon */}
+          <div className="flex flex-col text-xs w-[150px] h-[24px] align-center justify-center hover:scale-105 duration-200">
+            <Link
+              href={githubHref}
+              className="p-3 rounded-3xl font-semibold font-white bg-gray-200 flex gap-2 items-center justify-center"
+              target="_blank"
+            >
+              <FaGithub /> Open Github
+            </Link>
+          </div>
 
-        {/* GitHub Icon */}
-        <a
-          href={githubHref}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-        >
-          <FaGithub className="text-white text-4xl hover:scale-110 transition-transform duration-300" />
-        </a>
-      </div>
-      <div>
-        <p className="text-center">{desc}</p>
+          <div className="flex flex-col text-xs w-[150px] h-[24px] align-center justify-center hover:scale-105 duration-200">
+            <Link
+              href={liveHref}
+              className="p-3 rounded-3xl font-semibold font-white bg-gray-200 flex items-center justify-center gap-2"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faChrome} /> Open Live
+            </Link>
+          </div>
+        </div>
       </div>
 
-      <div className="flex align-center justify-center flex-wrap gap-1 p-2">
-        {techStack}
-      </div>
+      <div className="flex flex-col items-center justify-evenly border-red-500 h-[250px]">
+        <div>
+          <p className="">{desc}</p>
+        </div>
 
-      <div className="flex flex-col text-black gap-8 mb-4 text-xs h-[24px] align-center justify-center">
-        <Link
-          href={liveHref}
-          className="p-3 rounded-3xl font-semibold font-white bg-gray-200"
-          target="_blank"
-        >
-          <FontAwesomeIcon icon={faChrome} /> Open Live
-        </Link>
+        <div className="flex align-center justify-center flex-wrap gap-1 p-2">
+          {techStack}
+        </div>
       </div>
     </div>
   );
